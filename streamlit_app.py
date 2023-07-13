dd5616aef6e07a83db0eb48eada58bfea2a1d30c
sarangen2 <vsarankumar2003@gmail.com>
2023-07-13 01:23:31 -0400

    beautify

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 streamlit.text(fruityvice_response)
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
