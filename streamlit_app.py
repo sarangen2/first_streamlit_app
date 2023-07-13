c7c7f1ab46e4f6383699f9dd92d4984df9144f5e
sarangen2 <vsarankumar2003@gmail.com>
2023-07-12 23:17:19 -0400

    cool msg 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
