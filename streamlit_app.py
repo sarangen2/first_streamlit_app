7796c958c0e059eeb0c69214901edbb272630e0f
sarangen2 <vsarankumar2003@gmail.com>
2023-07-13 01:26:12 -0400

    add stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,14 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+
 
 
 
@@ -24,7 +31,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
+
 
 streamlit.text(fruityvice_response)
 
